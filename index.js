6c709c67e1d2b41d93caf3da5b942360c1d22ddf
nguyenminhtuan291 <nguyenminhtuan291@gmail.com>
2022-12-12 22:31:26 +0700

    frist add

---
+++ index.js
@@ -0,0 +1,63 @@
+//bài toán tính tiền lương nhân viên
+let lg = 100.000;
+let total1 = 0 // output;
+// thực hiện phép nhân
+total1 = lg * 30;
+
+console.log("bài tập tính lương =", total1);
+
+//bài toán tính trung bình tổng số thực 
+  let a = 5;
+
+  let b = 1;
+
+  let c = 4;
+
+  let d = 7;
+
+  let e = 8;
+
+  sum = a + b + c + d + e;
+
+  avg = sum/5;
+
+  console.log("trung bình =", avg);
+
+//quy đổi tiền
+
+let tien = 23.500;
+
+let qd = 0;
+
+qd = tien * 2;
+
+console.log("quy đổi tiền:", qd);
+
+//chu vi hình chữ nhật 
+
+let cd = 4;
+
+let cr = 5;
+
+let dv = cd * cr;
+
+console.log("diện tích =", dv);
+
+let cv = (cd + cr) * 2;
+
+console.log("chu vi =", cv);
+
+
+//tỉnh tổng 2 ký số 18
+
+let tu = 18;
+
+let ol = 0;
+
+ol = tu % 10;
+
+tu = Math.floor(tu / 10);
+
+ol += tu;
+
+console.log("tổng 2 kí số =", ol);
